adbae8b02cf9fd3e6ed0792a7ca1e25dc2a923c3
user <bharatcm70@gmail.com>
2020-03-29 01:43:29 +0530

    Completed Feature 3 Search by category

--- angular-bookstore/src/app/app.module.ts
+++ angular-bookstore/src/app/app.module.ts
@@ -1,21 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import {RouterModule,Routes} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookListsComponent } from './components/book-lists/book-lists.component';
 import { BookService } from './services/book.service';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
+const routes:  Routes=[
+{ path:'books', component:BookListsComponent},
+{ path:'category/:id', component:BookListsComponent},
+{ path:'', redirectTo:'/books',pathMatch:'full'},
+{ path:'**', component:PageNotFoundComponent}
+];
 @NgModule({
   declarations: [
     AppComponent,
-    BookListsComponent
+    BookListsComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [
     BookService
--- angular-bookstore/src/app/components/book-lists/book-lists.component.ts
+++ angular-bookstore/src/app/components/book-lists/book-lists.component.ts
@@ -1,48 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from 'src/app/common/book';
 import { BookService } from 'src/app/services/book.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-book-lists',
-  templateUrl: './book-lists.component.html',
+  // templateUrl: './book-lists.component.html',
+  templateUrl: './book-grid.component.html',
   styleUrls: ['./book-lists.component.css']
 })
 export class BookListsComponent implements OnInit {
 
-  constructor(private _bookService:BookService) { }
+  constructor(private _bookService:BookService,
+  private _activatedRoute :ActivatedRoute) { }
+  
+
 
   ngOnInit() {
     this.listBooks();
+    this._activatedRoute.paramMap.subscribe(()=>{
+      this.listBooks();
+    })
   }
 
-   books:Book[]=[
-  //   {
-  //   sku:"text-001",
-  //   name:"C# crash course",
-  //   description:"Learn C#",
-  //   unitPrice:900,
-  //   imageUrl:"",
-  //   active:true,
-  //   unitsInStock:100,
-  //   createdOn:new Date(),
-  //   updatedOn:null,
-  // },
-  // {
-  //   sku:"text-101",
-  //   name:"C Programming Language",
-  //   description:"Learn C",
-  //   unitPrice:200,
-  //   imageUrl:"",
-  //   active:true,
-  //   unitsInStock:20,
-  //   createdOn:new Date(),
-  //   updatedOn:null,
-  // },
-
-  ]
+   books:Book[]=[];
+   currentCategoryId:number;
 
   listBooks(){
-    this._bookService.getBooks().subscribe(
+
+    const hasCategoryId:boolean=this._activatedRoute.snapshot.paramMap.has('id');
+
+    if(hasCategoryId){
+      this.currentCategoryId=+this._activatedRoute.snapshot.paramMap.get('id');
+    }else
+    {
+      this.currentCategoryId=1;
+    }
+
+    this._bookService.getBooks(this.currentCategoryId).subscribe(
       data=>this.books=data
     )
 
